01e3f43a8bc16d8b80d5a9558ea69a645b75bfdc
Bradford-Meziere <coleokc@gmail.com>
2021-07-06 16:12:24 -0500

    Did some stuff

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
 // Your code goes here
+const signButton = document.querySelector('#first')
+const signButtonDos = document.querySelector('#second')
+const signButtonTres = document.querySelector('#last')
+const homeNav = document.querySelector('nav')
+const funBusTitle = document.querySelector('h1.logo-heading')
+//
+
+//
+signButton.onclick = function(event) {
+    console.log('First Button')
+}
+signButtonDos.onclick = function(event) {
+    console.log('Second Button')
+}
+signButtonTres.onclick = function(event) {
+    console.log('Last Button')
+}
+//
+
+//
+signButton.addEventListener('click', function(event){
+    this.style.backgroundColor = 'red'
+})
+signButton.addEventListener('mouseover', function(event){
+    this.style.backgroundColor = 'Green'
+})
+//
+
+//
+signButtonDos.addEventListener('click', function(event){
+    this.style.backgroundColor = 'orange'
+})
+signButtonDos.addEventListener('mouseover', function(event){
+    this.style.backgroundColor = 'Green'
+})
+//
+
+//
+signButtonTres.addEventListener('mouseover', function(event){
+    this.style.backgroundColor = 'Green'
+})
+signButtonTres.addEventListener('click', function(event){
+    this.style.backgroundColor = 'blue'
+})
+//
+
+//
+funBusTitle.addEventListener('mouseover', function(event){
+    this.style.backgroundColor = 'blue'
+})
