a1c71e9abd52944a7be2b518be190b7800ab1313
Bradford-Meziere <coleokc@gmail.com>
2021-07-06 17:17:46 -0500

    Did some more stuff

--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@
 const signButton = document.querySelector('#first')
 const signButtonDos = document.querySelector('#second')
 const signButtonTres = document.querySelector('#last')
-const homeNav = document.querySelector('nav')
+const homeNav = document.querySelector('nav .nav-link:nth-of-type(1)')
+const aboutNav = document.querySelector('nav .nav-link:nth-of-type(2)')
+const blogNav = document.querySelector('nav .nav-link:nth-of-type(3)')
 const funBusTitle = document.querySelector('h1.logo-heading')
+const firstPara = document.querySelector('header.intro')
+
 //
 
 //
@@ -47,5 +51,23 @@ signButtonTres.addEventListener('click', function(event){
 
 //
 funBusTitle.addEventListener('mouseover', function(event){
-    this.style.backgroundColor = 'blue'
+    this.style.fontSize = '90px'
+})
+//
+
+homeNav.addEventListener('dblclick', function(event){
+    this.style.backgroundColor = 'maroon'
+})
+
+aboutNav.addEventListener('wheel', function(event){
+    this.style.fontSize = '20px'
+})
+
+blogNav.addEventListener('click', function(event){
+    this.style.color = 'Green'
+    this.style.backgroundColor = 'Black'
+})
+
+firstPara.addEventListener('dblclick', function(event){
+    this.style.backgroundColor = 'pink'
 })
